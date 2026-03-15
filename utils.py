6a7bfda0da8e31444702d28c89b3635dcbad51e6
Sudhanshu Rai <sudhanshurai183@gmail.com>
2026-03-16 03:44:14 +0530

    minor gpu log

--- utils.py
+++ utils.py
@@ -27,7 +27,7 @@ def get_device() -> torch.device:
     if torch.cuda.is_available():
         device = torch.device("cuda")
         gpu_name = torch.cuda.get_device_name(0)
-        gpu_mem = torch.cuda.get_device_properties(0).total_mem / 1e9
+        gpu_mem = torch.cuda.get_device_properties(0).total_memory / 1e9
         logging.getLogger("smiles_iupac").info(
             f"Using device: cuda ({gpu_name}, {gpu_mem:.1f} GB)"
         )
